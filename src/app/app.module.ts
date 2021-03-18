8dd8d0fddd5159b81af560965df16b789a42086f
ShenSouza <ds052798ts@gmail.com>
2021-03-18 19:28:25 -0300

    feat(body): Implementado foto e tabela com links redes sociais #4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
+import { CorpoComponent } from './corpo/corpo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    RodapeComponent
+    RodapeComponent,
+    CorpoComponent
   ],
   imports: [
     BrowserModule,
